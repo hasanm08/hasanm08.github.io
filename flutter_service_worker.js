278e4cdba71fe9105a8802dd6a97f0673f8713a6
AmirHassan Amirmahani <hasanmahani08@gmail.com>
2020-11-05 20:32:18 +0330

    update about page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0aad3ebd41a4d6b78f5a0d57688b09e1",
-"/": "0aad3ebd41a4d6b78f5a0d57688b09e1",
-"main.dart.js": "cf0a44abe7d6728ddf51dfa2113c0ad1",
+"index.html": "12538f54fc243797109465f5211ed590",
+"/": "12538f54fc243797109465f5211ed590",
+"main.dart.js": "df2676666675f045029de9f71b624c22",
 "manifest.json": "246490102f6169c962f051509c988a18",
 "version.json": "6ee4937b434f4eeb18b78d8a3c76e50e"
 };
