ef8aba08de00223945d716c4e9f29cac01edd0b4
hasanm08 <hasanm08@users.noreply.github.com>
2022-04-10 05:59:09 +0000

    deploy: f854b92e0bd556d25ddd8396ee79c5bdbd95f37b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "b1ea651c3e09484586c571180b115f52",
-"index.html": "6ef02ee1784aa7fdae537850b512fb43",
-"/": "6ef02ee1784aa7fdae537850b512fb43",
+  "main.dart.js": "7600296b369079ce9964d9decca8c516",
+"index.html": "37bf4830440c23962701ff3a0eda8c25",
+"/": "37bf4830440c23962701ff3a0eda8c25",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "59764c8d51419e5b6b9981b6437c582e",
 "assets/FontManifest.json": "f1f80d900c76bd88612e57507f067436",
