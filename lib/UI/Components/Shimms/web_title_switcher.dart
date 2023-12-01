20ec91e6e89799b1e01fec0ff596e46a0f418650
Amirhassan <hasanmahani08@gmail.com>
2023-12-01 11:28:09 +0330

    a2hs added

--- lib/UI/Components/Shimms/web_title_switcher.dart
+++ lib/UI/Components/Shimms/web_title_switcher.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
+import 'dart:js' as js;
 
 import 'package:flutter/material.dart';
 
@@ -24,6 +25,9 @@ class _WebTitleSwitcherState extends State<WebTitleSwitcher> {
     // Register a blur event listener on the window object
     html.window.addEventListener('blur', _handleBlurEvent);
     html.window.addEventListener('focus', _handleFocusEvent);
+
+    // A2HS functionality
+    listenToBeforeInstallPromptEvent();
   }
 
   @override
@@ -52,7 +56,27 @@ class _WebTitleSwitcherState extends State<WebTitleSwitcher> {
     final String title = _isTabActive ? "Hi what's up :)" : "Come back ;(";
     html.document.title = title;
   }
+void listenToBeforeInstallPromptEvent() {
+  html.window.on['beforeinstallprompt'].listen((event) {
+    // Prevent the mini-infobar from appearing on mobile
+    event.preventDefault();
 
+    // Create an install button that, when triggered, prompts the user to install the app
+    html.ButtonElement installButton = html.ButtonElement()
+      ..text = "Add to Home Screen"
+      ..style.position = "fixed"
+      ..style.bottom = "10px"
+      ..style.left = "10px";
+
+    // Add the button to the body
+    html.document.body?.append(installButton);
+
+    // When the button is clicked, trigger the prompt
+    installButton.onClick.listen((_) {
+      // Cast the event to JsObject to invoke the prompt method
+      js.JsObject.fromBrowserObject(event).callMethod('prompt');
+    });
+  });
   @override
   Widget build(BuildContext context) {
     return widget.child;
