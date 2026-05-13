6d429dde5a6acde0b2f3abb884873acd8f248613
Amirhassan <Amirhasan.a@bit24.info>
2026-05-13 14:08:44 -0400

    Removed service worker version configuration from flutter_bootstrap.js to streamline loading settings for the web application.

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,13 +1,7 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-// Replaced at build time with a version string when offline-first SW is enabled.
-const serviceWorkerVersion = null;
-
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: serviceWorkerVersion,
-  },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
     await appRunner.runApp();
