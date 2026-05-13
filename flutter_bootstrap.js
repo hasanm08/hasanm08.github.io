727be4e7f714f3aa7d090ac441b332bf2515ae4a
hasanm08 <hasanm08@users.noreply.github.com>
2026-05-13 18:49:16 +0000

    deploy: 086f87bbffa962561316b985e1d8de43c38c4c1f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 
 _flutter.loader.load({
